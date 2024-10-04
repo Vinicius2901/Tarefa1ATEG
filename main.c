a682be2e39e34359218ca4eb8324cbc0090b8aa9
Vinícius Giroti <121032547+Vinicius2901@users.noreply.github.com>
2024-10-04 18:57:37 -0300

    Add files via upload
    Correção do aux como int;
    Mudança para pegar as posições dos mínimos e máximos das distâncias euclidianas;
    Escritura dos resultados em um arquivo csv.

--- main.c
+++ main.c
@@ -5,7 +5,8 @@
 int main() {
     int i, j;
     Flor flores[150];
-    FILE *f = fopen("C:\\Users\\vinic\\OneDrive\\Documents\\UDESC\\4 Semestre\\TEG\\PercursosECaminhos\\Tarefa1A\\IrisDataset.csv", "rt");
+    // Abertura e coleta dos dados do arquivo csv
+    FILE *f = fopen("IrisDataset.csv", "rt");
     if( f != NULL ) {
         char lixo[50];
         fscanf(f, "%[^\n]", lixo );
@@ -13,69 +14,102 @@ int main() {
             for( j = 0; j < 11; j++ ) {
                 flores[i].tipo[j] = '\0';
             }
-            fscanf(f, "\n%10[^,],", flores[i].tipo); // Lê até 10 caracteres ou até a vírgula
+            fscanf(f, "\n%[^,],", flores[i].tipo); // Lê até 10 caracteres ou até a vírgula
             fscanf( f, "%lf,%lf,%lf,%lf", &flores[i].sl, &flores[i].sw, &flores[i].pl, &flores[i].pw );
         }
         fclose(f);
 
-    // double distEuc[150][150], distEucNorm[150][150];
-    double **distEuc = malloc( 150*sizeof(double*) );
-    for( i = 0; i < 150; i++ ) {
-        distEuc[i] = malloc(150*sizeof(double));
-    }
-    double distEucNorm[150][150];
-    double aux = 0, max = 0, min = 0;
-    for( i = 0; i < 150; i++ ) {
-        distEuc[i][i] = 0;
-        for( j = i+1; j < 150; j++ ) {
-            aux = distEucl( &flores[i], &flores[j] );
-            distEuc[i][j] = aux;
-            distEuc[j][i] = aux;
-            if( aux > max ) {
-                max = aux;
-                if( i == 0 ) {
-                    min = aux;
+        // double distEuc[150][150], distEucNorm[150][150];
+        // Inicialização da matriz de distâncias euclidianas e cálculo delas entre as flores
+        double **distEuc = malloc( 150*sizeof(double*) );
+        for( i = 0; i < 150; i++ ) {
+            distEuc[i] = malloc(150*sizeof(double));
+        }
+        double aux = 0;
+        int iMaxEuc = 0, jMaxEuc = 0, iMinEuc = 0, jMinEuc = 0;
+        for( i = 0; i < 150; i++ ) {
+            distEuc[i][i] = 0;
+            for( j = i+1; j < 150; j++ ) {
+                aux = distEucl( &flores[i], &flores[j] );
+                distEuc[i][j] = aux;
+                distEuc[j][i] = aux;
+                // Pega a posição do máximo euclidiano encontrado e do mínimo, caso seja a primeira iteração
+                if( aux > distEuc[iMaxEuc][iMinEuc] ) {
+                    iMaxEuc = i;
+                    jMaxEuc = j;
+                    if( i == 0 ) {
+                        iMinEuc = i;
+                        jMinEuc = j;
+                    }
+                }
+                // Pega a posição do minimo euclidiano
+                if( aux < distEuc[iMinEuc][jMinEuc] ) {
+                    iMinEuc = i;
+                    jMinEuc = j;
                 }
             }
-            if( aux < min ) {
-                min = aux;
-            }
         }
-    }
-    for( i = 0; i < 150; i++ ) {
-        distEucNorm[i][i] = 0;
-        for( j = i+1; j < 150; j++ ) {
-            aux = distEuclNorm( distEuc, i, j, min, max );
-            distEucNorm[i][j] = aux;
-            distEucNorm[j][i] = aux;
-            if( aux > max ) {
-                max = aux;
-                if( i == 0 ) {
-                    min = aux;
+
+        // Inicialização da matriz de distâncias euclidianas normalizadas e cálculo delas entre as flores
+        double distEucNorm[150][150];
+        int iMaxNorm = 0, jMaxNorm = 0, iMinNorm = 0, jMinNorm = 0;
+        for( i = 0; i < 150; i++ ) {
+            distEucNorm[i][i] = 0;
+            for( j = i+1; j < 150; j++ ) {
+                aux = distEuclNorm( distEuc, i, j, distEuc[iMinEuc][jMinEuc], distEuc[iMaxEuc][jMaxEuc] );
+                distEucNorm[i][j] = aux;
+                distEucNorm[j][i] = aux;
+                // Pega a posição do maior euclidiano normalizado e do menor, caso seja a primeira iteração
+                if( aux > distEucNorm[iMaxNorm][jMaxNorm] ) {
+                    iMaxNorm = i;
+                    jMaxNorm = j;
+                    if( i == 0 ) {
+                        iMinNorm = i;
+                        jMinNorm = j;
+                    }
+                }
+                // Pega a posição do mínimo euclidiano normalizado
+                if( aux < distEucNorm[iMinNorm][jMinNorm] ) {
+                    iMinNorm = i;
+                    jMinNorm = j;
                 }
             }
-            if( aux < min ) {
-                min = aux;
-            }
         }
-    }
-    int matrizAdj[150][150];
-    for( i = 0; i < 150; i++ ) {
-        matrizAdj[i][i] = 0;
-        for( j = i+1; j < 150; j++ ) {
-            if( distEucNorm[i][j] <= 0.3 ) {
-                matrizAdj[i][j] = 1;
-                matrizAdj[j][i] = 1;
-            } else {
-                matrizAdj[i][j] = 0;
-                matrizAdj[j][i] = 0;
+
+        // Inicialização da matriz adjacência e ligação dos nodos caso a distância euclidiana normalizada seja <= 0,3
+        int matrizAdj[150][150];
+        for( i = 0; i < 150; i++ ) {
+            matrizAdj[i][i] = 0;
+            for( j = i+1; j < 150; j++ ) {
+                if( distEucNorm[i][j] <= 0.3 ) {
+                    matrizAdj[i][j] = 1;
+                    matrizAdj[j][i] = 1;
+                } else {
+                    matrizAdj[i][j] = 0;
+                    matrizAdj[j][i] = 0;
+                }
+                //printf("%i ", matrizAdj[i][j]);
             }
-            printf("%.1lf ", distEuc[i][j]);
+            //printf("\n");
         }
-        printf("\n");
-    }
+        FILE *data = fopen("data.csv", "wt");
+        if( data != NULL ) {
+            fprintf( data, "150,%.1lf,%i,%i,%.1lf,%i,%i,%.1lf,%i,%i,%.1lf,%i,%i\n", distEuc[iMaxEuc][jMaxEuc], iMaxEuc, jMaxEuc,
+            distEuc[iMinEuc][jMinEuc], iMinEuc, jMinEuc, distEucNorm[iMaxNorm][jMaxNorm], iMaxNorm, jMaxNorm, distEucNorm[iMinNorm][jMinNorm]);
+            for( i = 0; i < 150; i++ ) {
+                for( j = i+1; j < 150; j++ ) {
+                    if( matrizAdj[i][j] == 1 )
+                        fprintf( data, "%i,%i\n", i, j);
+                }
+            }
+            fclose(data);
+        } else {
+            printf("Erro ao computar os dados");
+            return 1;
+        }
     } else {
         printf("Erro na abertura de arquivo\n");
         return 1;
     }
+    return 0;
 }
