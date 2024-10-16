3ce69ddf8082f2f5510f9eee53c11b8b54237160
FelipeJojima <fknjojima@gmail.com>
2024-10-16 10:21:22 -0300

    Update and rename distancias.c to funcoes.c

--- funcoes.c
+++ funcoes.c
@@ -1,4 +1,4 @@
-#include "distancias.h"
+#include "funcoes.h"
 
 double distEucl( Flor *f1, Flor *f2 ) {
     return sqrt((f2->sl - f1->sl)*(f2->sl - f1->sl) + (f2->sw - f1->sw)*(f2->sw - f1->sw) + (f2->pl - f1->pl)*(f2->pl - f1->pl) + (f2->pw - f1->pw)*(f2->pw - f1->pw) );
